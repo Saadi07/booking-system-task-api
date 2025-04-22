18a310beff50787bc7865bb4acc430ae70320285
dev-saad09 <saadiawan09@gmail.com>
2025-04-23 01:49:37 +0500

    minor fixes and added env file

--- cron/index.js
+++ cron/index.js
@@ -5,7 +5,12 @@ const cron = require("node-cron");
 
 const expirePendingBookings = require("./expirePendingBookings.cron");
 
-cron.schedule("*/5 * * * *", () => {
-    log.info("Running booking expiration job...");
+const CRON_EXPRESSION = process.env.BOOKING_EXPIRY_CRON || "*/2 * * * *";
+
+cron.schedule(CRON_EXPRESSION, () => {
+    const currentTime = new Date().toISOString();
+    log.info(`Running booking expiration job at ${currentTime}...`);
     expirePendingBookings();
 });
+
+log.info(`Cron job scheduled to run every 5 minutes: ${CRON_EXPRESSION}`);
